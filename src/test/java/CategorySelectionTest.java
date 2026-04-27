f48ffd0a14b075e940e4c32cd465754c85afaf64
Ruth Ramirez <ruthralcazar@gmail.com>
2026-04-26 21:51:22 -0700

    unit test for category selection

---
+++ src/test/java/CategorySelectionTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class CategorySelectionTest {
+
+    @Test
+    void testCategorySelectionExists() {
+        //checks that the class can be loaded
+        assertNotNull(new CategorySelectionTest());
+    }
+}
